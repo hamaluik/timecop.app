4f6c264eff8d7609b053f9ed2901a21ba309d7ed
Kenton Hamaluik <kenton@hamaluik.ca>
2020-03-03 12:23:17 -0700

    translated HTML to all supported languages

--- translate.sh
+++ translate.sh
@@ -7,5 +7,7 @@ BODY=$(echo "{\"contents\": [], \"mimeType\": \"text/html\", \"sourceLanguageCod
 URL="https://translation.googleapis.com/v3/projects/$(cat gcc-credentials.json| jq -cr '.project_id')/locations/global:translateText"
 RESPONSE=$(curl -s -X POST -H "$AUTHORIZATION" -H "Content-Type: application/json; charset=utf-8" -d "$BODY" "$URL")
 TRANSLATION=$(echo "$RESPONSE" | jq -rM ".translations[0].translatedText")
-HTML=$(echo $TRANSLATION | sed -e "s/lang=en/lang=$TARGET/g")
-echo $HTML > public/$TARGET/$SOURCE_FILE
+# attempt to add an HTML Lang attribute, but sometimes it just gives `null` files
+#HTML=$(echo $TRANSLATION | sed -e "s/<html>/<html lang=$TARGET>/g")
+#echo $HTML > public/$TARGET/$SOURCE_FILE
+echo $TRANSLATION > public/$TARGET/$SOURCE_FILE
